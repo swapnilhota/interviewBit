0703c3f59b288f276aa6ad8b643cd9e9cb519ffd
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-05-24 14:17:40 +0530

    Create Intersection Of Sorted Arrays.cpp

---
+++ Two Pointers/Intersection Of Sorted Arrays.cpp
@@ -0,0 +1,28 @@
+vector<int> Solution::intersect(const vector<int> &A, const vector<int> &B) {
+    
+    vector<int> ans;
+    
+    int i=0;
+    int j=0;
+    
+    while(i<A.size() && j<B.size())
+    {
+        if(A[i]==B[j])
+        {
+            ans.push_back(A[i]);
+            i++;
+            j++;
+        }
+        else if(A[i]>B[j])
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    
+    return ans;
+    
+}
