56a31b6db099d56046702a725442587e63803e87
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-05-24 12:41:49 +0530

    Create Min XOR Value.cpp

---
+++ Bit Manipulation/Min XOR Value.cpp
@@ -0,0 +1,14 @@
+int Solution::findMinXor(vector<int> &A) {
+    
+    sort(A.begin(), A.end());
+    
+    int ans = INT_MAX;
+    
+    for(int i=1; i<A.size(); i++)
+    {
+        ans = min(ans, A[i]^A[i-1]);
+    }
+    
+    return ans;
+    
+}
