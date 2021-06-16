21118a13e6ec4f2e13136f617b4de0a50873212a
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-06-16 17:12:06 +0530

    Create Valid BST from Preorder.cpp

---
+++ Tree Data Structure/Valid BST from Preorder.cpp
@@ -0,0 +1,22 @@
+int Solution::solve(vector<int> &A) {
+    
+    stack<int> s;
+    int root = INT_MIN;
+    
+    for(int i=0; i<A.size(); i++)
+    {
+        if(root>A[i])
+        {
+            return 0;
+        }
+        while(!s.empty() && s.top()<A[i])
+        {
+            root=s.top();
+            s.pop();
+        }
+        s.push(A[i]);
+    }
+    
+    return 1;
+    
+}
