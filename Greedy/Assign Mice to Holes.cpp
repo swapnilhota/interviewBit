1b5507735d3493258ef8f51a8d25e245cd053f51
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-10-09 15:03:01 +0530

    Create Assign Mice to Holes.cpp

---
+++ Greedy/Assign Mice to Holes.cpp
@@ -0,0 +1,48 @@
+/*
+Problem Description
+
+There are N Mice and N holes that are placed in a straight line. Each hole can accomodate only 1 mouse.
+
+The positions of Mice are denoted by array A and the position of holes are denoted by array B.
+
+A mouse can stay at his position, move one step right from x to x + 1, or move one step left from x to x − 1. Any of these moves consumes 1 minute.
+
+Assign mice to holes so that the time when the last mouse gets inside a hole is minimized.
+
+
+
+Problem Constraints
+1 <= N <= 105
+
+-109 <= A[i], B[i] <= 109
+
+
+
+Input Format
+First argument is an integer array A.
+
+Second argument is an integer array B.
+
+
+
+Output Format
+Return an integer denoting the minimum time when the last nouse gets inside the holes.
+*/
+
+int Solution::mice(vector<int> &A, vector<int> &B) {
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    int ans=0;
+    for(int i=0; i<A.size(); i++)
+    {
+        if(A[i]>B[i])
+        {
+            ans = max(ans, A[i]-B[i]);
+        }
+        else
+        {
+            ans = max(ans, B[i]-A[i]);
+        }
+    }
+    return ans;
+}
